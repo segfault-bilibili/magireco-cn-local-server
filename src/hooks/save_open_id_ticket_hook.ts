c3de64e5e3f91d91c3dcebfd0b92881780c3a031
segfault-bilibili <segfault_bilibili@qq.com>
2022-09-26 16:55:04 +0800

    fix capture open_id and ticket

--- src/hooks/save_open_id_ticket_hook.ts
+++ src/hooks/save_open_id_ticket_hook.ts
@@ -10,6 +10,53 @@ export class saveOpenIdTicketHook extends saveResponseBodyHook {
         super(params, urlRegEx, paramKey);
     }
 
+    private readonly openIdKeyRegEx = /^User-Id-[0-9a-zA-Z]+$/i;
+    private readonly ticketKeyRegEx = /^Ticket$/i;
+
+    matchRequest(
+        method?: string,
+        url?: URL,
+        httpVersion?: string,
+        headers?: http.IncomingHttpHeaders | http2.IncomingHttpHeaders
+    ): boolean {
+        const tag = "saveOpenIdTicketHook";
+
+        const isGameLogin = super.matchRequest(method, url, httpVersion, headers);
+        if (isGameLogin) return true;
+
+        const magirecoRegEx = /^(http|https):\/\/l\d+-[0-9a-z\-]+-mfsn\d*\.bilibiligame\.net\//;
+        const isMagiReco = url != null && url.href.match(magirecoRegEx);
+        if (!isMagiReco) return isGameLogin;
+
+        if (headers == null) return isGameLogin;
+        let open_id: string | undefined, ticket: string | undefined;
+        for (let key in headers) {
+            let val = headers[key];
+            if (typeof val !== 'string' || val === "") continue;
+            if (key.match(this.openIdKeyRegEx)) open_id = val;
+            if (key.match(this.ticketKeyRegEx)) ticket = val;
+            if (open_id != null && ticket != null) break;
+        }
+        if (open_id == null || ticket == null) {
+            return isGameLogin;
+        }
+
+        //console.log(`${tag} got open_id and ticket from request`);//DEBUG
+        let last = this.params.openIdTicket;
+        if (
+            last != null
+            && last.open_id === open_id
+            && last.ticket === ticket
+        ) {
+            return isGameLogin;//no need to update
+        }
+
+        console.log(`${tag} got open_id and ticket from request`);
+        this.params.save({ key: this.paramKey, val: { open_id: open_id, ticket: ticket } })
+            .then(() => console.log(`${tag} saved to paramKey=[${this.paramKey}]`));
+        return false; // no need to capture request/response body
+    }
+
     onMatchedResponse(
         statusCode?: number,
         statusMessage?: string,
@@ -17,17 +64,47 @@ export class saveOpenIdTicketHook extends saveResponseBodyHook {
         headers?: http.IncomingHttpHeaders | (http2.IncomingHttpHeaders & http2.IncomingHttpStatusHeader),
         body?: Buffer | string
     ): void {
-        const tag = "saveOpenIdTicket";
+        const tag = "saveOpenIdTicketHook";
+
+        // save ticket
+        let ticket: string | undefined;
+        if (headers != null) {
+            for (let key in headers) {
+                let val = headers[key];
+                if (typeof val !== 'string' || val === "") continue;
+                if (key.match(this.ticketKeyRegEx)) ticket = val;
+                if (ticket != null) break;
+            }
+        }
+        if (ticket == null) {
+            console.error(`${tag} ticket == null`);
+            return;
+        }
+
+        // save open_id
         if (typeof body === 'string') {
             try {
                 let resp = JSON.parse(body);
-                if (resp.resultCode === 'success' && resp.data != null && typeof resp.data.open_id === 'string') {
-                    console.log(`${tag} login successful`);
-                    this.params.save({ key: this.paramKey, val: resp })
-                        .then(() => console.log(`${tag} saved to paramKey=[${this.paramKey}]`));
-                } else {
-                    console.error(`${tag} login unsuccessful resp.resultCode=[${resp.resultCode}]`);
+                if (resp == null) {
+                    console.error(`${tag} response body is null`);
+                    return;
                 }
+                if (resp.resultCode !== 'success') {
+                    console.error(`${tag} login unsuccessful resp.resultCode=[${resp.resultCode}]`);
+                    return;
+                }
+                if (
+                    typeof resp.data.open_id !== 'string'
+                    || resp.data.open_id === ""
+                ) {
+                    console.error(`${tag} open_id is empty`);
+                    return;
+                }
+
+                console.log(`${tag} login successful`);
+                resp.data["ticket"] = ticket;
+                this.params.save({ key: this.paramKey, val: resp.data })
+                    .then(() => console.log(`${tag} saved to paramKey=[${this.paramKey}]`));
             } catch (e) {
                 console.error(`${tag} error parsing resp`, e);
             }
