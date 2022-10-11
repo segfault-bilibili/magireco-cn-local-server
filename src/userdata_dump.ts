a381d616d90c90b41447b82e9dae6c0620037e85
segfault-bilibili <segfault_bilibili@qq.com>
2022-10-12 03:21:49 +0800

    utc+8

--- src/userdata_dump.ts
+++ src/userdata_dump.ts
@@ -110,13 +110,13 @@ export class userdataDmp {
         return this.dateTimeNumberStr(Number(loginTime));
     }
     private dateTimeNumberStr(time: number): string {
-        const date = new Date(time);
-        const year = String(date.getFullYear()).padStart(4, "0");
-        const mon = String(date.getMonth() + 1).padStart(2, "0");
-        const day = String(date.getDate()).padStart(2, "0");
-        const hour = String(date.getHours()).padStart(2, "0");
-        const min = String(date.getMinutes()).padStart(2, "0");
-        const sec = String(date.getSeconds()).padStart(2, "0");
+        const date = new Date(time + 8 * 60 * 60 * 1000);
+        const year = String(date.getUTCFullYear()).padStart(4, "0");
+        const mon = String(date.getUTCMonth() + 1).padStart(2, "0");
+        const day = String(date.getUTCDate()).padStart(2, "0");
+        const hour = String(date.getUTCHours()).padStart(2, "0");
+        const min = String(date.getUTCMinutes()).padStart(2, "0");
+        const sec = String(date.getUTCSeconds()).padStart(2, "0");
         return year + mon + day + hour + min + sec;
     }
 
